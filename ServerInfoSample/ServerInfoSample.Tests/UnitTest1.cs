9b6cc1f88f236f07aa53a15aea5db7f2797b3c95
Ahmed Somarribas <asomarribasd@gmail.com>
2019-09-23 13:07:58 -0600

    Sample Proyect Tests - Added one sample test to fill later

---
+++ ServerInfoSample/ServerInfoSample.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace ServerInfoSample.Providers.Tests
+{
+    public class InformationFactoryProvidersTests
+    {
+        [Fact]
+        public void GeoIpFactory_CreateReturns_GeoIPProvider()
+        {
+
+        }
+
+        [Fact]
+        public void PingFactory_CreateReturns_PingProvider()
+        {
+
+        }
+    }
+}
